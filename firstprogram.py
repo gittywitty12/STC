96db14092b5da310fd77791eae3444ff710a2432
gittywitty12 <100278356+gittywitty12@users.noreply.github.com>
2022-03-06 12:33:05 +0530

    Create firstprogram.py

---
+++ firstprogram.py
@@ -0,0 +1,18 @@
+# the author of this program is Mr. Akshay kalyan
+# i am writing this program on 7th march 2022
+
+#=======================thank you for reading=================================
+#how do we write a single line comment
+# by using a #(hash/ pound) sign anywhere
+
+#this is a single line comment
+
+
+#how do we write a single line comment
+#byusing triple single quote's sign
+
+'''
+this is a multi line 
+comment
+'''
+
